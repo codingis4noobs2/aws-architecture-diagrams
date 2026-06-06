10fd584fa4d388de30769000f3c617d4ebabe603
Parth Shah <4gameparth@gmail.com>
2026-06-06 21:22:52 +0530

    Change Terraform state file key path
    Updated the key path for the Terraform state file.

--- bastion-host-arch/backend.tf
+++ bastion-host-arch/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "youholdbutter"
-    key            = "bastion/terraform.tfstate"
+    key            = "./terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
